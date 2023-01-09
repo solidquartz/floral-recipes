cc5685e231b312cbc0b583b247848b7bd462be98
Aky Mac Donald <akymacd@gmail.com>
2023-01-09 15:41:30 -0500

    fixed db errors

--- server/db/schema/03_arrangements.sql
+++ server/db/schema/03_arrangements.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS arrangements CASCADE;
 
 CREATE TABLE arrangements (
-  id INT GENERATED ALWAYS AS IDENTITY,
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   project_id INT REFERENCES projects(id) ON DELETE CASCADE,
   arrangement_name VARCHAR(255) NOT NULL,
   arrangement_quantity INTEGER NOT NULL
