cee7c8947400c80e02fb82a4361bd06b6d741042
Aky Mac Donald <akymacd@gmail.com>
2023-01-16 13:30:24 -0500

    Add project button and styling

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,29 +1,68 @@
 // import { Link } from "react-router-dom";
 
 import {
-  Box,
   Button,
   Flex,
-  FormControl,
   Heading,
   Input,
+  Spacer,
+  Table,
+  TableContainer,
+  Tbody,
+  Th,
+  Thead,
+  Tr,
 } from "@chakra-ui/react";
+import { BiBookAdd } from "react-icons/bi";
+import { BiSearchAlt } from "react-icons/bi";
 import { Header } from "./components";
 
 export const Projects: React.FC = () => {
   return (
     <>
+      {/* Navbar */}
       <Header />
 
+      {/* Title and Search */}
       <Flex m="auto" p="20px" justify="space-around">
-        <Heading>Projects</Heading>
+        <Flex>
+          <Flex pr="20px" align-items="center">
+            <Heading>Projects</Heading>
+          </Flex>
+          <Flex>
+            <Button
+              variant="outline"
+              colorScheme="cyan"
+              leftIcon={<BiBookAdd />}
+            >
+              Create Project
+            </Button>
+          </Flex>
+        </Flex>
 
         <Flex>
           <Input type="text" placeholder="Search" />
-          <Button colorScheme="pink" variant="outline" size="md">
+          <Button colorScheme="pink" variant="outline" leftIcon={<BiSearchAlt/>}>
             Search
           </Button>
         </Flex>
+      </Flex>
+
+      {/* Table of Projects */}
+      <Flex justify="center">
+        <TableContainer>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>Project Name</Th>
+                <Th>Event Date</Th>
+                <Th>Last Updated</Th>
+                <Th>Status</Th>
+              </Tr>
+            </Thead>
+            <Tbody></Tbody>
+          </Table>
+        </TableContainer>
       </Flex>
     </>
   );
