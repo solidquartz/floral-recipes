312dc08987fbecab96ec90a431c0b67eea9c1f39
Aky Mac Donald <akymacd@gmail.com>
2023-01-16 13:08:20 -0500

    Add title and search bar

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,5 +1,30 @@
 // import { Link } from "react-router-dom";
 
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
+import { Header } from "./components";
+
 export const Projects: React.FC = () => {
-  return <div>Projects</div>;
+  return (
+    <>
+      <Header />
+
+      <Flex m="auto" p="20px" justify="space-around">
+        <Heading>Projects</Heading>
+
+        <Flex>
+          <Input type="text" placeholder="Search" />
+          <Button colorScheme="pink" variant="outline" size="md">
+            Search
+          </Button>
+        </Flex>
+      </Flex>
+    </>
+  );
 };
