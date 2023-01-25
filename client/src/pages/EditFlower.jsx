c8d3893e42a6815934d4510b40f140b2547e5167
Aky Mac Donald <akymacd@gmail.com>
2023-01-25 16:39:21 -0500

    Created edit floral page and component

--- client/src/pages/EditFlower.jsx
+++ client/src/pages/EditFlower.jsx
@@ -1,5 +1,16 @@
 // import { Link } from "react-router-dom";
 
+import { Flex, Heading } from "@chakra-ui/react";
+import { Header } from "./components";
+
 export const EditFlower = () => {
-  return <div>Edit Flower</div>;
+  return (
+    <>
+      <Header />
+
+      <Flex justify="center" direction="column" align="center">
+        <Heading>Edit Floral</Heading>
+      </Flex>
+    </>
+  );
 };
