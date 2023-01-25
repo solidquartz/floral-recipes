5b698cb94b7975d5e533f098a08a24a5abcaa160
Aky Mac Donald <akymacd@gmail.com>
2023-01-25 15:36:29 -0500

    Projects are ordered by last updated

--- server/routes/projectsRoutes.js
+++ server/routes/projectsRoutes.js
@@ -5,7 +5,7 @@ const db = require('../configs/db.config');
 //gets all projects with all data
 app.get('/', async (req, res) => {
   try {
-    const results = await db.query("SELECT * FROM projects");
+    const results = await db.query("SELECT * FROM projects ORDER BY last_updated");
     res.status(200).json({
       status: "success",
       data: {
