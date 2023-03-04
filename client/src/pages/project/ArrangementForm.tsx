2a9071d9535e161414394c20c7a1ad28f70746f7
Aky Mac Donald <akymacd@gmail.com>
2023-03-04 12:41:03 -0500

    edit arrangement and edit project

--- client/src/pages/project/ArrangementForm.tsx
+++ client/src/pages/project/ArrangementForm.tsx
@@ -1,8 +1,10 @@
 import {
   Box,
   Button,
+  ButtonGroup,
   Flex,
   Heading,
+  Select,
   Table,
   TableContainer,
   Tbody,
@@ -15,40 +17,35 @@ import {
 import { FiInfo } from "react-icons/fi";
 import { Dropdown, Icon } from "../shared";
 import { getOrderSize, getTotalCost } from "./helpers";
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { TextField } from '../shared';
+import { FieldArray, Form, Formik } from "formik";
+import * as Yup from "yup";
+import { TextField } from "../shared";
 
-
-export const ArrangementForm = ({
-  arrangement,
-  flowers
-}) => {
-
+export const ArrangementForm = ({ flowers, project, editing }) => {
   //shape data
   // const flowersInArrangement = useMemo(() => {
-  // 	if (!flowers) {
-  // 		return [];
-  // 	}
-  // 	return arrangement.flowers.map((x) => {
-  // 		const flower = flowers.find((f) => f.id === x.flower_id);
-  // 		const stemPrice = parseFloat(flower.stem_price);
-  // 		const stemQuantity = parseFloat(x.stem_quantity);
-  // 		const rounded = getOrderSize(stemQuantity, flower.rounded_up);
-  // 		const baseCost = stemPrice * x.stem_quantity;
-  // 		const roundedCost = rounded * stemPrice;
+  //  if (!flowers) {
+  //    return [];
+  //  }
+  //  return arrangement.flowers.map((x) => {
+  //    const flower = flowers.find((f) => f.id === x.flower_id);
+  //    const stemPrice = parseFloat(flower.stem_price);
+  //    const stemQuantity = parseFloat(x.stem_quantity);
+  //    const rounded = getOrderSize(stemQuantity, flower.rounded_up);
+  //    const baseCost = stemPrice * x.stem_quantity;
+  //    const roundedCost = rounded * stemPrice;
 
-  // 		return {
-  // 			name: flower.flower_name,
-  // 			stem_price: stemPrice,
-  // 			quantity: x.stem_quantity,
-  // 			rounded,
-  // 			base_cost: parseFloat(baseCost),
-  // 			rounded_cost: roundedCost,
-  // 			markup200: roundedCost * 2,
-  // 			markup250: roundedCost * 2.5,
-  // 		};
-  // 	});
+  //    return {
+  //      name: flower.flower_name,
+  //      stem_price: stemPrice,
+  //      quantity: x.stem_quantity,
+  //      rounded,
+  //      base_cost: parseFloat(baseCost),
+  //      rounded_cost: roundedCost,
+  //      markup200: roundedCost * 2,
+  //      markup250: roundedCost * 2.5,
+  //    };
+  //  });
   // }, [flowers]);
 
   // const totalCost = getTotalCost(flowersInArrangement);
@@ -57,7 +54,20 @@ export const ArrangementForm = ({
   // const totalMarkup250 = costAllArrangements * 2.5;
 
   //initial values
-  const initialValues = {};
+  const initialValues = {
+    arrangements: [
+      {
+        arrangement_name: "Arrangement Name",
+        arrangement_quantity: 0,
+        flowers: [
+          {
+            flower_id: 0,
+            stem_quantity: 0,
+          },
+        ],
+      },
+    ],
+  };
 
   return (
     <Box borderBottom="1px solid #ececec" py="1rem" my="1rem">
@@ -67,108 +77,167 @@ export const ArrangementForm = ({
         // validationSchema={}
         // onSubmit={}
       >
-        {(formik) => (
-          <>
-            <Flex flexDirection="column">
-              <Heading size="md" textTransform="capitalize">
-                Arrangement name (take from state)
-              </Heading>
-              <Flex flexDirection="column" paddingTop="5px" width="500px">
-                <TextField
-                  name="arrangement_name"
-                  type="text"
-                  placeholder="Enter Name"
-                />
-                <TextField
-                  name="arrangement_quantity"
-                  type="text"
-                  placeholder="0"
-                  label="Quantity"
-                />
-              </Flex>
+        {({ values, errors, isSubmitting, isValid }) => (
+          <Form>
+            <FieldArray
+              name="arrangements"
+              {...(arrayHelpers) => (
+                <>
+                  {values.arrangements.map((_, index) => (
+                    <>
+                      <Flex flexDirection="column">
+                        <Heading size="md" textTransform="capitalize">
+                          {/* {arrangement?.arrangement_name} */}
+                        </Heading>
 
-              <TableContainer whiteSpace="normal" maxW="1080px">
-                <Table size="lg">
-                  <Thead>
-                    <Tr>
-                      <Th>Flower Type</Th>
-                      <Th></Th>
-                      <Th>Stems per Piece</Th>
-                      <Th>Price per Stem</Th>
-                      <Th>Min Order Size</Th>
-                      <Th>
-                        <Icon
-                          icon={<FiInfo />}
-                          placement="end"
-                          tooltipText="Total cost for the stem order of each flower type before rounding up"
+                        <Flex
+                          flexDirection="column"
+                          paddingTop="5px"
+                          width="500px"
                         >
-                          Total
-                        </Icon>
-                      </Th>
-                      <Th>Rounded Up Total</Th>
-                      <Th>Markup 200%</Th>
-                      <Th>Markup 250%</Th>
-                    </Tr>
-                  </Thead>
+                          <TextField
+                            name={`arrangements.${index}.arrangement_name`}
+                            type="text"
+                            placeholder="Enter Name"
+                          />
+                          <TextField
+                            name={`arrangements.${index}.arrangement_quantity`}
+                            type="text"
+                            placeholder="0"
+                            label="Quantity"
+                          />
+                        </Flex>
 
-                  <Tbody>
-                    {/* {flowersInArrangement.map((x, idx) => ( 
-                      formik with field array*/}
-                    <Tr>
-                      <Td colSpan="2">
-                        <Dropdown name="flowerName" placeholder="Flower">
-                          <option value="rose">Rose</option>
-                        </Dropdown>
-                      </Td>
-                      <Td>
-                        <TextField name="stem_quantity" type="text" />
-                      </Td>
-                      <Td textAlign="right">$1.00</Td>
-                      <Td>5</Td>
-                      <Td textAlign="right">$2.00</Td>
-                      <Td textAlign="right">$5.00</Td>
-                      <Td textAlign="right">$00</Td>
-                      <Td textAlign="right">$00</Td>
-                    </Tr>
-                    {/* ))} */}
-                  </Tbody>
-                </Table>
-              </TableContainer>
-              <Flex paddingTop="10px">
-                <Button colorScheme="pink" variant="outline">
-                  Add Flower
-                </Button>
-              </Flex>
-            </Flex>
+                        <TableContainer whiteSpace="normal" maxW="1080px">
+                          <Table size="lg">
+                            <Thead>
+                              <Tr>
+                                <Th>Flower Type</Th>
+                                <Th></Th>
+                                <Th>Price per Stem</Th>
+                                <Th>Stems per Piece</Th>
+                                <Th>Min Order Size</Th>
+                                <Th>
+                                  <Icon
+                                    icon={<FiInfo />}
+                                    placement="end"
+                                    tooltipText="Total cost for the stem order of each flower type before rounding up"
+                                  >
+                                    Total
+                                  </Icon>
+                                </Th>
+                                <Th>Rounded Up Total</Th>
+                                <Th>Markup 200%</Th>
+                                <Th>Markup 250%</Th>
+                              </Tr>
+                            </Thead>
 
-            <Flex
-              flexDirection="column"
-              alignItems="flex-end"
-              pr="30px"
-              pt="20px"
-            >
-              <Text fontSize="md" textTransform="uppercase" textAlign="right">
-                {/* <b>Cost per Arrangement</b>: ${totalCost.toFixed(2)} */}
-              </Text>
-              <Text fontSize="md" textTransform="uppercase" textAlign="right">
-                {/* <b>Arrangement Quantity</b>: {arrangement.arrangement_quantity} */}
-              </Text>
-              <Text fontSize="md" textTransform="uppercase" textAlign="right">
-                {/* <b>Total (All Arrangements)</b>: ${costAllArrangements.toFixed(2)} */}
-              </Text>
+                            <Tbody>
+                              <Tr>
+                                <Td colSpan="2">
+                                  <Select
+                                    name={"flowers.flower_name"}
+                                    placeholder="Flower"
+                                  >
+                                    {flowers.map((flower) => (
+                                      <option
+                                        value={flower.flower_name}
+                                        key={flower.id}
+                                      >
+                                        {flower.flower_name}
+                                      </option>
+                                    ))}
+                                  </Select>
+                                </Td>
+                                <Td textAlign="right">$1.00</Td>
+                                <Td>
+                                  <TextField name="stem_quantity" type="text" />
+                                </Td>
+                                <Td>5</Td>
+                                <Td textAlign="right">$2.00</Td>
+                                <Td textAlign="right">$5.00</Td>
+                                <Td textAlign="right">$00</Td>
+                                <Td textAlign="right">$00</Td>
+                              </Tr>
+                              {/* ))} */}
+                            </Tbody>
+                          </Table>
+                        </TableContainer>
 
-              {/* Put below into accordion */}
-              <Text fontSize="md" textTransform="uppercase" textAlign="right">
-                {/* <b>Total 200% Markup</b>: ${totalMarkup200.toFixed(2)} */}
-              </Text>
-              <Text fontSize="md" textTransform="uppercase" textAlign="right">
-                {/* <b>Total 250% Markup</b>: ${totalMarkup250.toFixed(2)} */}
-              </Text>
-            </Flex>
-          </>
+                        <Flex paddingTop="10px" justifyContent="space-between">
+                          <Flex>
+                            <ButtonGroup>
+                              <Button colorScheme="pink" variant="outline">
+                                Add Flower
+                              </Button>
+                              <Button colorScheme="pink">Save Flower</Button>
+                            </ButtonGroup>
+                          </Flex>
+                          <Flex>
+                            <Button colorScheme="teal">Save Arrangement</Button>
+                          </Flex>
+                        </Flex>
+                      </Flex>
+
+                      <Flex
+                        flexDirection="column"
+                        alignItems="flex-end"
+                        pr="30px"
+                        pt="20px"
+                      >
+                        <Text
+                          fontSize="md"
+                          textTransform="uppercase"
+                          textAlign="right"
+                        >
+                          {/* <b>Cost per Arrangement</b>: ${totalCost.toFixed(2)} */}
+                        </Text>
+                        <Text
+                          fontSize="md"
+                          textTransform="uppercase"
+                          textAlign="right"
+                        >
+                          {/* <b>Arrangement Quantity</b>: {arrangement.arrangement_quantity} */}
+                        </Text>
+                        <Text
+                          fontSize="md"
+                          textTransform="uppercase"
+                          textAlign="right"
+                        >
+                          {/* <b>Total (All Arrangements)</b>: ${costAllArrangements.toFixed(2)} */}
+                        </Text>
+
+                        {/* Put below into accordion */}
+                        <Text
+                          fontSize="md"
+                          textTransform="uppercase"
+                          textAlign="right"
+                        >
+                          {/* <b>Total 200% Markup</b>: ${totalMarkup200.toFixed(2)} */}
+                        </Text>
+                        <Text
+                          fontSize="md"
+                          textTransform="uppercase"
+                          textAlign="right"
+                        >
+                          {/* <b>Total 250% Markup</b>: ${totalMarkup250.toFixed(2)} */}
+                        </Text>
+                      </Flex>
+
+                      <Flex paddingTop="10px" justifyContent="flex-end">
+                        <Button colorScheme="teal" variant="outline">
+                          Add Arrangement
+                        </Button>
+                      </Flex>
+                    </>
+                  ))}
+                </>
+              )}
+            />
+            <pre>{JSON.stringify({ values, errors }, null, 4)}</pre>
+          </Form>
         )}
       </Formik>
     </Box>
   );
 };
-
--- client/src/pages/project/EditProjectComponent.tsx
+++ client/src/pages/project/EditProjectComponent.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, ButtonGroup, Flex, VStack } from "@chakra-ui/react";
+import { Box, Button, ButtonGroup, Flex, Text, VStack } from "@chakra-ui/react";
+import dayjs from "dayjs";
 import { Formik } from "formik";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
@@ -20,12 +21,14 @@ export const EditProjectComponent = () => {
   useEffect(() => {
     const fetchProject = async () => {
       const response = await api.get(`/projects/${id}`);
-      setProjectName(response.data.data.project.project_name);
-      setEventDate(response.data.data.project.event_date);
+      setProjectName(response.data.data.project.name);
+      const formattedEventDate = dayjs(
+        response.data.data.project.event_date
+      ).format("YYYY-MM-DD");
+      setEventDate(formattedEventDate);
     };
     fetchProject();
   }, []);
-
 
   const initialValues = {
     project_name: projectName,
@@ -39,12 +42,12 @@ export const EditProjectComponent = () => {
         enableReinitialize={true}
         validationSchema={Yup.object({
           project_name: Yup.string().required("Please enter a project name"),
-          stem_price: Yup.date().required("Please enter a date"),
+          event_date: Yup.date().required("Please enter a date"),
         })}
         onSubmit={async (values) => {
           const response = await api.patch(`/projects/${id}`, {
             project_name: values.project_name,
-            event_date: values.event_date,
+            event_date: dayjs(values.event_date).format(),
           });
           state.upsertProject(response.data.data.project);
           window.location.href = `/projects/${response.data.data.project.id}/details`;
@@ -65,24 +68,16 @@ export const EditProjectComponent = () => {
                 <TextField
                   name="project_name"
                   type="text"
-                  placeholder={projectName}
                   label="Project Name"
                 />
-                <TextField
-                  name="event_date"
-                  type="date"
-                  placeholder={eventDate}
-                  label="Event Date"
-                />
+                <TextField name="event_date" type="date" label="Event Date" />
 
                 {/* Buttons */}
                 <ButtonGroup spacing="6">
-                  <Link to="/projects">
+                  <Link to={`/projects/${id}/details`}>
                     <Button>Cancel</Button>
                   </Link>
-                  <Button
-                    type="submit"
-                    colorScheme="pink">
+                  <Button type="submit" colorScheme="pink">
                     Update
                   </Button>
                 </ButtonGroup>
